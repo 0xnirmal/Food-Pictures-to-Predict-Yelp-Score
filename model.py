a102ba7bc7e5cfb98b0b70d1d2d0dcfe578f37ee
Nirmal Krishnan <nkrishn9@jhu.edu>
2018-05-03 16:18:11 -0400

    testing

--- model.py
+++ model.py
@@ -110,7 +110,7 @@ train_loader = DataLoader(train_dataset, batch_size=args.batch_size,
                         shuffle=True, num_workers=2)
 
 val_loader = DataLoader(val_dataset, batch_size=256,
-                        shuffle=True, num_workers=2)
+                        shuffle=True, num_workers=0)
 
 
 # val_img, val_label = val_loader[0]
