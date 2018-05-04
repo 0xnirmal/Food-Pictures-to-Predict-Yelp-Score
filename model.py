9bf4b3f57adf4a5254af760d9f074dd2271b5a40
Nirmal Krishnan <nkrishn9@jhu.edu>
2018-05-04 18:21:03 -0400

    autoencoder

--- model.py
+++ model.py
@@ -189,7 +189,7 @@ class AutoEncoder(nn.Module):
 	def __init__(self):
 		super(AutoEncoder, self).__init__()
 		self.encoder = nn.Sequential(
-			nn.Conv2d(1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
+			nn.Conv2d(3, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
 			nn.ReLU(True),
 			nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
 			nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 8, 3, 3
@@ -201,7 +201,7 @@ class AutoEncoder(nn.Module):
 			nn.ReLU(True),
 			nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
 			nn.ReLU(True),
-			nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+			nn.ConvTranspose2d(8, 3, 2, stride=2, padding=1),  # b, 1, 28, 28
 			nn.Tanh()
 		)
 
