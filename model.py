ebdf6d2e8a6078393f4ce7f124f157254934a019
Nirmal Krishnan <nkrishn9@jhu.edu>
2018-05-04 18:25:48 -0400

    autoencoder

--- model.py
+++ model.py
@@ -199,9 +199,9 @@ class AutoEncoder(nn.Module):
 		self.decoder = nn.Sequential(
 			nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
 			nn.ReLU(True),
-			nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
-			nn.ReLU(True),
-			nn.ConvTranspose2d(8, 3, 2, stride=2, padding=2),  # b, 1, 28, 28
+			nn.ConvTranspose2d(16, 3, 3, stride=3, padding=1),  # b, 8, 15, 15
+			# nn.ReLU(True),
+			# nn.ConvTranspose2d(8, 3, 2, stride=2, padding=1),  # b, 1, 28, 28
 			nn.Tanh()
 		)
 
