a87fb58ae881fba72185d1e0b1af2cf342fa7386
Nirmal Krishnan <nkrishn9@jhu.edu>
2018-05-04 18:49:36 -0400

    autoencoder

--- model.py
+++ model.py
@@ -191,22 +191,19 @@ class AutoEncoder(nn.Module):
 		self.encoder = nn.Sequential(
 			nn.Conv2d(3, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
 			nn.ReLU(True),
-			# nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
 			nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 8, 3, 3
 			nn.ReLU(True),
-			# nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
 		)
 		self.decoder = nn.Sequential(
 			nn.ConvTranspose2d(8, 16, 3, stride=2, padding=1),  # b, 16, 5, 5
 			nn.ReLU(True),
 			nn.ConvTranspose2d(16, 3, 3, stride=3, padding=1, output_padding=1),  # b, 8, 15, 15
-			nn.ReLU(True)
-			# nn.ConvTranspose2d(8, 3, 2, stride=2, padding=1),  # b, 1, 28, 28
-			# nn.Tanh()
+			nn.Tanh()
 		)
 
 	def forward(self, x):
 		x = self.encoder(x)
+		print(x.shape)
 		x = self.decoder(x)
 		return x
 
